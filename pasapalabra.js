61ea75424fec3cbfd031ec34e526d83007e32d96
Apolbaq <emilianopolanco5@gmail.com>
2021-11-17 23:08:53 +0100

    Number 11 Wheel

--- pasapalabra.js
+++ pasapalabra.js
@@ -203,7 +203,7 @@ function findScreenSize(){
                 { value: '1turn', duration: durationAnimation, delay: delayWheel }
             ],
             scale: [
-                { value: 14, duration: durationAnimation, delay: delayWheel }
+                { value: 11, duration: durationAnimation, delay: delayWheel }
             ],
         });
     } else {
